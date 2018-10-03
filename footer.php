bceeba29c84f9ae275604060fa3d091af12482ac
nueng55110 <31878323+nueng55110@users.noreply.github.com>
2018-10-03 13:47:15 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
-<table width=100% bgcolor=#FFCCCC><tr><td>
-<span style="font-size:20px;text-align:center;">
-<td colspan="3"><Marquee scrollamount="15"><font size=5 face="Impact">สามารถติดต่อได้ที่ เบอร์โทรศัพท์ 098-581-5473</Marquee></td>
-</span>
+<html><head><title>burin</title>
+<meta http-equiv="content-type"
+content="text/html;charset=tis-620" />
+</head>
+<body bgcolor=#FFFFCC>
+<table width=100% bgcolor=pink><tr><td>
+<span style="font-size:30px;">ยินดีต้อนรับ</span>
 </td></tr></table>
-</body></html>
